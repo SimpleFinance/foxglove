d15b4b1c11f7b7403879a5056f46787ecbc368a2
Jake Davis <jake.davis5989@gmail.com>
2015-01-20 18:43:48 +0000

    Allow a local testing option that will just no-op

--- src/foxglove/core.clj
+++ src/foxglove/core.clj
@@ -10,18 +10,21 @@
 
    It is strongly recommended you partially apply this function, and the
    parameters are ordered with this specifically in mind."
-  [url services sender options message & {:keys [exceptions?]
-                                          :or {exceptions? false}}]
-  (let [payload {:message  message
-                 :sender   sender
-                 :options  options
-                 :services services}
-        full-url (str url "/notify-api/message")]
-    (http/post full-url {:form-params payload
-                         :content-type :json
-                         :coerce :always
-                         :as :json
-                         :throw-exceptions exceptions?})))
+  [url services sender options message & {:keys [exceptions? local-testing]}]
+  (when-not local-testing
+    (let [payload {:message  message
+                   :sender   sender
+                   :options  options
+                   :services services}
+          full-url (str url "/notify-api/message")]
+      (http/post full-url {:form-params payload
+                           :content-type :json
+                           :coerce :always
+                           :as :json
+                           :throw-exceptions exceptions?})
+      (log/infof
+        "url=%s message=%s sender=%s services=%s" full-url message sender services)
+      (log/debugf "options=%s" options))))
 
 (defn services
   "GET /notify-api/services
